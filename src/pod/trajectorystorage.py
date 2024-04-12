022566484599c497c2c6ea7333ad2ca9ecfdc40c
benjamin <benjaminHucko@gmail.com>
2024-04-12 17:22:42 +0200

    different storage system

--- src/pod/trajectorystorage.py
+++ src/pod/trajectorystorage.py
@@ -1,30 +1,26 @@
-from src.resultwriter.csvwriter import CsvWriter
+import jax.numpy as jnp
 
 
 class TrajectoryStorage:
     def __init__(self, filename):
         headers = ["frame_stack", "action", "reward", "next_frame"]
-        self.csv_writer = CsvWriter(filename, headers)
         self.frame_stack = []
         self.actions = []
         self.rewards = []
         self.next_frames = []
         self.size = 0
+        self.index = 0
         # flush data into file
 
-    def flush_buffer(self):
-        data = self._prepare_data()
-        self.csv_writer.store_data(data)
+    def store(self):
+        data = {"frame_stack": self.frame_stack, "action": self.actions,
+                "reward": self.rewards, "next_frame": self.next_frames}
+        jnp.savez(f"data_{self.index}", **data)
+        self.index += 1
         self.rewards = []
         self.frame_stack = []
         self.actions = []
         self.next_frames = []
-
-    def _prepare_data(self):
-        data = [[stack, action, reward, nextFrame]
-                for stack, action, reward, nextFrame
-                in zip(self.frame_stack, self.actions, self.rewards, self.next_frames)]
-        return data
 
     # add new data
     def add_input(self, frame_stack, action):
@@ -35,6 +31,6 @@ class TrajectoryStorage:
     def add_teacher(self, reward, next_frame):
         self.rewards.append(reward)
         self.next_frames.append(next_frame)
-        if self.size == 50:
-            self.flush_buffer()
+        if self.size == 500:
+            self.store()
             self.size = 0
