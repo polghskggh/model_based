1ce048955ca99795cc3208a0900d1edee501c91a
benjamin <benjaminHucko@gmail.com>
2024-05-31 14:12:58 +0200

    logging change

--- src/utils/save_name.py
+++ src/utils/save_name.py
@@ -6,4 +6,4 @@ from src.singletons.hyperparameters import Args
 def save_name():
     args = Args().args
     run_name = f"{args.seed}_{time.asctime(time.localtime(time.time())).replace('  ', ' ').replace(' ', '_')}"
-    return f"{args.env}/{args.env}/{args.algorithm}/{run_name}"
+    return f"{args.algorithm}/{args.env}/{run_name}"
