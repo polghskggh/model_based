67ea7d333e5a714e744dc49e90224bd2dd69ecc9
benjamin <benjaminHucko@gmail.com>
2024-06-09 10:14:51 +0200

    debug

--- src/modelbased.py
+++ src/modelbased.py
@@ -1,14 +1,6 @@
-import gymnasium as gym
-import jax.numpy as jnp
-from jax import lax
-
 from src.agent.agent import Agent
-from src.enviroment import Shape
-from src.modelfree import interact, model_free_train_loop
-from src.pod.storage import TransitionStorage, store
+from src.modelfree import model_free_train_loop
 from src.singletons.hyperparameters import Args
-from src.singletons.step_traceker import StepTracker
-from src.singletons.writer import Writer
 from src.worldmodel.worldmodelinterface import WorldModelInterface
 
 
@@ -19,11 +11,17 @@ def sample_env(agent, envs):
     return envs.storage
 
 
+def interact_world_model(agent, world_model):
+    init_state, _ = world_model.reset()
+    agent.receive_state(init_state)
+    model_free_train_loop(agent, world_model, False, Args().args.sim_trajectory_length)
+
+
 def model_based_train_loop(agent: Agent, world_model: WorldModelInterface, env):
     storage = sample_env(agent, env)
     world_model.update(storage)
 
     for update in range(Args().args.model_updates):
-        model_free_train_loop(agent, world_model, False, Args().args.sim_trajectory_length)
+        interact_world_model(agent, world_model)
 
 
