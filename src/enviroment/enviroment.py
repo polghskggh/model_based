682814becd240aba2ce2c7bc080be285937710ec
benjamin <benjaminHucko@gmail.com>
2024-06-10 13:48:32 +0200

    modifications

--- src/enviroment/enviroment.py
+++ src/enviroment/enviroment.py
@@ -47,6 +47,7 @@ def make_breakout() -> gym.Env:
     Create the Breakout environment
     """
     env: gym.Env = gym.make("ALE/Breakout-v5", render_mode="rgb_array", frameskip=4)
+    apply_common_wrappers(env)
     return env
 
 
