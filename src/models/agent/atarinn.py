26c14d6b8954475889fbbb4eacbece107f5a480c
benjamin <benjaminHucko@gmail.com>
2024-06-21 11:02:22 +0200

    dqn changes

--- src/models/agent/atarinn.py
+++ src/models/agent/atarinn.py
@@ -1,16 +1,18 @@
 import flax.linen as nn
 import jax.numpy as jnp
 from jax import Array
+from rlax import one_hot
 
 from src.models.helpers import convolution_layer_init, linear_layer_init
 
 
 class AtariNN(nn.Module):
     input_dimensions: tuple
+    action_n: int
     output_dimensions: tuple
 
     @nn.compact
-    def __call__(self, x: Array):
+    def __call__(self, x: Array, action: int):
         x = convolution_layer_init(32, 8, 4)(x)
         x = nn.relu(x)
         x = convolution_layer_init(64, 4, 2)(x)
@@ -19,6 +21,14 @@ class AtariNN(nn.Module):
         x = nn.relu(x)
         x = jnp.reshape(x, (x.shape[0], -1))
         x = linear_layer_init(512)(x)
+        x = nn.relu(x)
+
+        action = one_hot(action, self.action_n)
+        x = jnp.append(x, action, axis=-1)
+
+        x = linear_layer_init(300, std=1)(x)
+        x = nn.relu(x)
+        x = linear_layer_init(100, std=1)(x)
         x = nn.relu(x)
         x = linear_layer_init(1, std=1)(x)
         return x
