99246edee03090c401926e782a3e01e5fbd8f506
benjamin <benjaminHucko@gmail.com>
2024-06-23 10:48:42 +0200

    dreamer debug

--- src/models/dreamer/observation.py
+++ src/models/dreamer/observation.py
@@ -4,6 +4,7 @@ import jax.numpy as jnp
 from src.models.autoencoder.decoder import Decoder
 from src.models.autoencoder.logitslayer import LogitsLayer
 from src.models.helpers import linear_layer_init
+from src.singletons.hyperparameters import Args
 from src.utils.activationfuns import activation_function_dict
 
 
@@ -16,25 +17,14 @@ class ObservationModel(nn.Module):
 
     def setup(self):
         self.activation_fun = activation_function_dict[self.activation_function]
-        self.layers = 4
-        self.features = 256
-        self.decoder = Decoder(64)
+        self.decoder = Decoder(Args().args.bottleneck_dims[-1])
 
     @nn.compact
     def __call__(self, belief, state):
         hidden = jnp.append(belief, state, axis=-1)
         hidden = linear_layer_init(features=self.embedding_size)(hidden)
-        hidden = hidden.reshape(-1, 2, 2, self.embedding_size // 4)
+        hidden = hidden.reshape(-1, *Args().args.bottleneck_dims)
 
         reconstructed = self.decoder(hidden, None)
         reconstructed = LogitsLayer()(reconstructed)
         return reconstructed
-
-    def scaled_features(self, layer_id: int):
-        if layer_id < 4:
-            return self.features
-        if layer_id == 4:
-            return self.features // 2
-        if layer_id == 5:
-            return self.features // 4
-
