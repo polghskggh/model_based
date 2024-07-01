34f681645c32d929496b68956aa21e242695a034
benjamin <benjaminHucko@gmail.com>
2024-07-01 13:40:54 +0200

    dreamer debug

--- src/trainer/dreamertrainer.py
+++ src/trainer/dreamertrainer.py
@@ -37,18 +37,19 @@ class DreamerTrainer(Trainer):
 
         batch_size = Args().args.batch_size
         print("HUGEEE", observations.shape)
+        grad_fn = value_and_grad(self.loss_fun, 1, True)
         for _ in range(Args().args.num_epochs):
             for env_idx in range(0, observations.shape[1]):
                 last_belief, last_state = initial_belief[env_idx], initial_state[env_idx]
                 for start_idx in range(0, observations.shape[0], batch_size):
                     batch_slice = slice(start_idx, start_idx + batch_size)
-                    (loss, aux), grads = value_and_grad(self.loss_fun, 1, True)(apply_funs, params,
-                                                                                jnp.expand_dims(observations[batch_slice, env_idx], 1),
-                                                                                jnp.expand_dims(actions[batch_slice, env_idx], 1),
-                                                                                jnp.expand_dims(rewards[batch_slice, env_idx], 1),
-                                                                                jnp.expand_dims(dones[batch_slice, env_idx], 1),
-                                                                                jnp.expand_dims(last_state, 0), 
-                                                                                jnp.expand_dims(last_belief, 0), rng=rng)
+                    (loss, aux), grads = grad_fn(apply_funs, params,
+                                                 jnp.expand_dims(observations[batch_slice, env_idx], 1),
+                                                 jnp.expand_dims(actions[batch_slice, env_idx], 1),
+                                                 jnp.expand_dims(rewards[batch_slice, env_idx], 1),
+                                                 jnp.expand_dims(dones[batch_slice, env_idx], 1),
+                                                 jnp.expand_dims(last_state, 0),
+                                                 jnp.expand_dims(last_belief, 0), rng=rng)
                     self.apply_grads(grads)
                     last_belief, last_state = aux["data"]
                     log(aux["info"])
@@ -65,7 +66,9 @@ class DreamerTrainer(Trainer):
         encoded_observations = apply_funs[key](params[key], observations, rngs=rng)
 
         print("encoded_shape", encoded_observations.shape)
+
         key = "representation"
+
         def scan_fn(carry, inputs):
             action, encoded_observation = inputs
             belief_carry, state_carry = carry
@@ -92,6 +95,7 @@ class DreamerTrainer(Trainer):
 
         key = "reward"
         reward_logits = apply_funs[key](params[key], beliefs, states)
+        print("reward_logits", reward_logits.shape, rewards.shape)
         reward_loss = reward_loss_fn(reward_logits, rewards)
 
         dones_loss = 0
