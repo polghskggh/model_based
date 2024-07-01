4c0f4351fedebca3e45e60212eccf41d53dde013
benjamin <benjaminHucko@gmail.com>
2024-07-01 13:49:12 +0200

    dreamer debug

--- src/trainer/dreamertrainer.py
+++ src/trainer/dreamertrainer.py
@@ -113,5 +113,5 @@ class DreamerTrainer(Trainer):
                 {
                     "info": {"observation_loss": observation_loss, "reward_loss": reward_loss, "kl_loss": kl_loss,
                              "dones_loss": dones_loss},
-                    "data": (belief, state)
+                    "data": (beliefs[-1], states[-1])
                 })
