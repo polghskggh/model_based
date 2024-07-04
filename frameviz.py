edae8c27aa44951dceed9870402c808dfe926baa
benjamin <benjaminHucko@gmail.com>
2024-07-04 20:58:10 +0200

    different debug info

--- frameviz.py
+++ frameviz.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     for i in range(1):
-        data = np.load(f'debug_{i}.npy')
+        data = np.load(f'target_{i}.npy')
+        print(data.shape)
         plt.imshow(data, cmap='gray')
         plt.savefig(f'debug_{i}.png')
