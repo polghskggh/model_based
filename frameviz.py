26eb518c6692d722abfe0e21d63869c606cf4352
benjamin <benjaminHucko@gmail.com>
2024-07-04 21:12:24 +0200

    different debug info

--- frameviz.py
+++ frameviz.py
@@ -5,5 +5,8 @@ if __name__ == '__main__':
     for i in range(1):
         data = np.load(f'target_{i}.npy')
         print(data.shape)
-        plt.imshow(data, cmap='gray')
+        plt.imshow(data.squeeze(), cmap='gray')
+        plt.savefig(f'target_{i}.png')
+        data = np.load(f'debug_{i}.npy')
+        plt.imshow(data.squeeze(), cmap='gray')
         plt.savefig(f'debug_{i}.png')
