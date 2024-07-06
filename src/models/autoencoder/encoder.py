4a5071b7919e3bd2c4844d1b1032c69bf6258075
benjamin <benjaminHucko@gmail.com>
2024-07-06 07:33:22 +0200

    debug encoder.py

--- src/models/autoencoder/encoder.py
+++ src/models/autoencoder/encoder.py
@@ -17,7 +17,6 @@ class Encoder(nn.Module):
 
     @nn.compact
     def __call__(self, x: Array) -> tuple:
-        x = jnp.astype(x, jnp.float32) / 255
         skip = []
         for layer_id in range(self.layers):
             skip.append(x)
