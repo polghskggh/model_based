2605756091fbef43377022b5d667a45ea77b72ce
benjamin <benjaminHucko@gmail.com>
2024-07-08 19:33:55 +0200

    debug prints dreamer

--- sample trajectory/frameviz.py
+++ sample trajectory/frameviz.py
@@ -3,11 +3,12 @@ from matplotlib import pyplot as plt
 
 def show(name):
     data = np.load(name + ".npy")
+    data = np.argmax(data, axis=-1)
     plt.imshow(data.squeeze(), cmap='gray')
     plt.savefig(name + ".png")
 
 
 if __name__ == '__main__':
     for i in range(50):
-        show(f'debug_simple_{i}')
+        show(f'debug_dreamer_{i}')
 
